c901027c9be0dcb74faa3700be5fa9bc7bb5ff4d
Hamim Ahmed <hamim.ahmed484@gmail.com>
2023-08-23 01:18:20 +0600

    header part done

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header-3');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 250){
+        header.classList.add('active');
+    }else{
+        header.classList.remove('active');
+    }
+
+}
