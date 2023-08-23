a2f3429858518213246cfd128aea8809d2125dbb
Hamim Ahmed <hamim.ahmed484@gmail.com>
2023-08-23 17:08:27 +0600

    home section added

--- js/script.js
+++ js/script.js
@@ -18,3 +18,15 @@ window.onscroll = () =>{
     }
 
 }
+
+var swiper = new Swiper(".home-slider", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    loop:true,
+  });
